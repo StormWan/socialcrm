42fc5ced2aeda6f58bf0b074b5e601dcd965e18b
wanwan <1623985631@qq.com>
2020-02-12 14:13:23 +0800

    4

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const pxtorem = require('postcss-pxtorem')
 
 module.exports = {
   // outputDir: 'docs',
-  publicPath: './',
+  publicPath: 'static/dist',
   // publicPath: process.env.NODE_ENV === 'production' ? '/static/dist' : '/',
   outputDir: 'static/dist',
   assetsDir: '',
